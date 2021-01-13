03d63c24a533eae21d6b27f58b291af9621603fa
Ricardo <ricardo10_2010@hotmail.com>
2021-01-13 15:07:46 -0300

    Gps done

--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./Pages/Home";
 import Orders from "./Pages/Orders";
+import OrderDetails from "./Pages/OrderDetails";
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,7 @@ function Routes() {
             >
                 <Stack.Screen name="Home" component={Home}></Stack.Screen>
                 <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+                <Stack.Screen name="OrderDetails" component={OrderDetails}></Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     )   
